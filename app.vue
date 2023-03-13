70cc1cf01cc9e437430323d1a5170529906ba5c3
ABBtagdan <tage.danielsson@abbgymnasiet.se>
2023-03-13 13:00:54 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+    <NuxtPage />
+</template>
+
+<style scoped>
+.hello {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 3rem;
+    padding: 10rem;
+}
+</style>
---
+++ pages/page_1.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="hello">
+        <ContentDoc />
+    </div>
+</template>
+
+<style scoped>
+.hello {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 3rem;
+    padding: 10rem;
+}
+</style>
