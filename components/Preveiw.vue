ef0d215ce5ddc1036a0304bf6b2ef3ee02cec389
ABBtagdan <tage.danielsson@abbgymnasiet.se>
2023-04-21 21:04:06 +0200

    gonna try something but this one works.

--- components/Preveiw.vue
+++ components/Preveiw.vue
@@ -1,14 +1,20 @@
 <template>
     <div class = "relative overflow-hidden mb-2 h-1/2 rounded-3xl">
-    <NuxtLink :to="post.slug" class="w-full group">
+    <NuxtLink :to="`/article/${post.slug}`" class="w-full group">
     <NuxtImg :src = "post.img" class="w-full h-full object-cover object-center blur-none group-hover:blur-sm transition" alt="post">
 
     </NuxtImg>
     <div class="absolute bottom-3 text-center group-hover:scale-110 text-2xl left-[50%] -translate-x-1/2 -translate-y-1/2 text-white">
         {{ post.title }}
     </div>
-    <div class ="absolute hidden text-center w-60% group-hover:block left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 text-white">
+    <div class ="absolute hidden text-center w-[60%] group-hover:block text-lg left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 text-white">
         {{ post.description }}
+    </div>
+    <div class ="absolute hidden text-center group-hover:block right-5 top-5 text-sm text-white">
+        {{ new Date(post.date).toLocaleDateString() }}
+    </div>
+    <div class ="absolute hidden text-center group-hover:block left-5 top-5 text-sm text-white">
+        {{ post.author }}        
     </div>
     </NuxtLink>
     </div>
@@ -19,5 +25,6 @@
 const { post } = defineProps({
     post: Object
 })
+
 
 </script>
