e88fb6f62a34c0027ee9efb4c6f3473b899c6bc1
ABBtagdan <tage.danielsson@abbgymnasiet.se>
2023-04-27 10:21:13 +0200

    to /

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <div>
     <header class="font-bold w-full h-[7vh] flex items-center font-sans">
         <nav class="container mx-auto flex px-6 justify-between text-black">
-            <a href="/" class="text-xl">Rytmer.se</a>
+            <NuxtLink to="/" class="text-xl">Rytmer.se</NuxtLink>
             <div class="flex gap-x-10">
             <NuxtLink to="/about"><div class="hover:scale-110">About</div></NuxtLink>
             <NuxtLink to="/contact"><div class="hover:scale-110">Contact</div></NuxtLink>
